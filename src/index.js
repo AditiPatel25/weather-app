e98c0efe48a08eb76419b26b396db41030c02a15
aditipatel <pate1452@purdue.edu>
2025-10-22 16:23:11 -0500

    finished front end ui

--- src/index.js
+++ src/index.js
@@ -5,24 +5,30 @@ console.log("App is running!");
 const searchButton = document.getElementById("searchButton");
 
 async function getWeather(query) {
-  const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${query}?key=A2N78FL6MR9RVEHPWHMHC62XF`,
-  );
-  const weatherData = await response.json();
-  console.log(weatherData);
-  console.log(processWeatherData(weatherData));
+    const response = await fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${query}?key=A2N78FL6MR9RVEHPWHMHC62XF`,
+    );
+    const weatherData = await response.json();
+    console.log(weatherData);
+    console.log(processWeatherData(weatherData));
 }
 
 searchButton.addEventListener("click", function () {
-  const query = document.querySelector(".search input").value;
-  getWeather(query);
+    const query = document.querySelector(".search input").value;
+    getWeather(query);
 });
 
 function processWeatherData(data) {
-  var high = data.days[0].tempmax;
-  console.log("high: " + high);
-  var low = data.days[0].tempmin;
-  console.log("low: " + low);
-  var current = data.days[0].temp;
-  console.log("current: " + current);
+    var high = data.days[0].tempmax;
+    console.log("high: " + high);
+    var low = data.days[0].tempmin;
+    console.log("low: " + low);
+    var current = data.days[0].temp;
+    console.log("current: " + current);
+    var feelslike = data.days[0].feelslike;
+    console.log("feelslike: " + feelslike);
+    var description = data.days[0].conditions;
+    console.log("description: " + description);
 }
+
+
