85d05c3449f64095190e852990e1cfea9daace0f
aditipatel <pate1452@purdue.edu>
2025-10-22 13:53:02 -0500

    added basic ui styling and api functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import "./styles.css";
 
 console.log("App is running!");
+
+const searchButton = document.getElementById("searchButton");
+
+async function getWeather(query) {
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${query}?key=A2N78FL6MR9RVEHPWHMHC62XF`,
+  );
+  const weatherData = await response.json();
+  console.log(weatherData);
+  console.log(processWeatherData(weatherData));
+}
+
+searchButton.addEventListener("click", function () {
+  const query = document.querySelector(".search input").value;
+  getWeather(query);
+});
+
+function processWeatherData(data) {
+  var high = data.days[0].tempmax;
+  console.log("high: " + high);
+  var low = data.days[0].tempmin;
+  console.log("low: " + low);
+  var current = data.days[0].temp;
+  console.log("current: " + current);
+}
--- webpack.common.js
+++ webpack.common.js
@@ -1,43 +1,42 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: {
-        app: './src/index.js',
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename:'index.html',
-            inject: 'head',
-            scriptLoading: 'defer',
-            title:'Production',
-        }),
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "head",
+      scriptLoading: "defer",
+      title: "Production",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-            }
-        ]
-    },
-    
+  },
 };
